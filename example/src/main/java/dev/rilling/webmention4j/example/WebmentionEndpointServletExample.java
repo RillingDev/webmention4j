d81df4cba349777b8a6203e8399487f74891502b
FelixRilling <contact@rilling.dev>
2022-06-26 13:10:09 +0200

    Removed error details from server.

--- example/src/main/java/dev/rilling/webmention4j/example/WebmentionEndpointServletExample.java
+++ example/src/main/java/dev/rilling/webmention4j/example/WebmentionEndpointServletExample.java
@@ -5,6 +5,7 @@ import dev.rilling.webmention4j.server.AbstractWebmentionEndpointServlet;
 import org.eclipse.jetty.server.CustomRequestLog;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Slf4jRequestLogWriter;
+import org.eclipse.jetty.server.handler.ErrorHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -40,6 +41,8 @@ public final class WebmentionEndpointServletExample {
 	private static void startServer(int port) {
 		Server server = new Server(port);
 
+		server.setErrorHandler(createErrorHandler());
+
 		server.setRequestLog(new CustomRequestLog(new Slf4jRequestLogWriter(), CustomRequestLog.EXTENDED_NCSA_FORMAT));
 
 		ServletHandler servletHandler = new ServletHandler();
@@ -54,12 +57,21 @@ public final class WebmentionEndpointServletExample {
 		}
 	}
 
+	@NotNull
+	private static ErrorHandler createErrorHandler() {
+		ErrorHandler errorHandler = new ErrorHandler();
+		errorHandler.setShowServlet(false);
+		errorHandler.setShowStacks(false);
+		return errorHandler;
+	}
+
 	@SuppressWarnings("serial")
 	public static class LoggingWebmentionEndpointServlet extends AbstractWebmentionEndpointServlet {
 
 		@Override
 		protected void handleWebmention(@NotNull Webmention webmention) {
-			LOGGER.info("Received Webmention from source '{}' with target '{}'.", webmention.source(),
+			LOGGER.info("Received Webmention from source '{}' with target '{}'.",
+				webmention.source(),
 				webmention.target());
 		}
 	}
