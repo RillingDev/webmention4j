7f31d7f5537c03147de3eb6e4e0176381b787193
FelixRilling <contact@rilling.dev>
2022-07-16 10:54:36 +0200

    Extracted HTML link analysis.

--- common/src/main/java/dev/rilling/webmention4j/common/util/HtmlUtils.java
+++ common/src/main/java/dev/rilling/webmention4j/common/util/HtmlUtils.java
@@ -8,6 +8,8 @@ import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Evaluator;
 
 import java.io.IOException;
 
@@ -29,12 +31,47 @@ public final class HtmlUtils {
 	 * @return The response as a HTML document.
 	 * @see #isHtml(HttpResponse)
 	 */
+	@NotNull
 	public static Document parse(@NotNull ClassicHttpResponse httpResponse) throws IOException {
 		try {
 			String body = EntityUtils.toString(httpResponse.getEntity());
 			return Jsoup.parse(body);
 		} catch (ParseException e) {
 			throw new IOException("Could not parse body.", e);
+		}
+	}
+
+	/**
+	 * Filters for HTML elements that are link-like.
+	 * This includes:
+	 * <ul>
+	 *     <li>anchor tags</li>
+	 *     <li>media tags</li>
+	 * </ul>
+	 */
+	public static class LinkLikeElementEvaluator extends Evaluator {
+
+		/**
+		 * Extracts the link from a link-like element.
+		 *
+		 * @param element Element to parse. Must be an element that {@link #matches(Element, Element)} was true for.
+		 * @return Link of this element. May not be a valid URI!
+		 */
+		@NotNull
+		public static String getLink(Element element) {
+			if ("a".equals(element.normalName())) {
+				return element.attr("href");
+			}
+			return element.attr("src");
+		}
+
+		@Override
+		public boolean matches(@NotNull Element root, @NotNull Element element) {
+			return switch (element.normalName()) {
+				case "a" -> element.hasAttr("href");
+				case "img", "video", "audio" -> element.hasAttr("src");
+				default -> false;
+			};
 		}
 	}
 }
--- server/src/main/java/dev/rilling/webmention4j/server/impl/verifier/HtmlVerifier.java
+++ server/src/main/java/dev/rilling/webmention4j/server/impl/verifier/HtmlVerifier.java
@@ -1,11 +1,10 @@
 package dev.rilling.webmention4j.server.impl.verifier;
 
 import dev.rilling.webmention4j.common.util.HtmlUtils;
+import dev.rilling.webmention4j.common.util.HtmlUtils.LinkLikeElementEvaluator;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.ContentType;
 import org.jetbrains.annotations.NotNull;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Evaluator;
 
 import java.io.IOException;
 import java.net.URI;
@@ -20,30 +19,14 @@ public class HtmlVerifier implements Verifier {
 
 	@Override
 	public boolean isValid(@NotNull ClassicHttpResponse httpResponse, @NotNull URI target) throws IOException {
-		return HtmlUtils.parse(httpResponse).selectFirst(new HtmlLinkEvaluator(target)) != null;
-	}
-
-	private static class HtmlLinkEvaluator extends Evaluator {
-		@NotNull
-		private final String targetUriString;
-
-		HtmlLinkEvaluator(@NotNull URI target) {
-			targetUriString = target.toString();
-		}
-
-		@Override
-		public boolean matches(@NotNull Element root, @NotNull Element element) {
+		return HtmlUtils.parse(httpResponse)
 			/*
 			 * Spec:
 			 * '[...] in an HTML5 document, the receiver should look for <a href="*">, <img href="*">,
 			 *  <video src="*"> and other similar links.'
-			 */
+			 */.select(new LinkLikeElementEvaluator()).stream().map(LinkLikeElementEvaluator::getLink)
 			// Note: The spec does state 'exact match', so strict equality is used rather than resolving the URLs.
-			return switch (element.normalName()) {
-				case "a" -> targetUriString.equals(element.attr("href"));
-				case "img", "video", "audio" -> targetUriString.equals(element.attr("src"));
-				default -> false;
-			};
-		}
+			.anyMatch(link -> target.toString().equals(link));
 	}
+
 }
