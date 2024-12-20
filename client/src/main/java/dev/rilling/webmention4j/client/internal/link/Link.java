0abe1c527eab23def7cd08a6270e5ef0346cb519
RillingDev <contact@rilling.dev>
2024-12-20 10:19:32 +0100

    Updated deps.

--- client/src/main/java/dev/rilling/webmention4j/client/internal/link/Link.java
+++ client/src/main/java/dev/rilling/webmention4j/client/internal/link/Link.java
@@ -6,7 +6,7 @@ import java.net.URI;
 import java.util.Set;
 
 /**
- * Simplified implementation of a `Link`:
+ * Simplified implementation of a `Link`, supporting only the URI and the 'rel' attribute:
  * <a href="https://datatracker.ietf.org/doc/html/rfc8288">https://datatracker.ietf.org/doc/html/rfc8288</a>
  * <p>
  * The URL is always in the resolved, absolute form.
